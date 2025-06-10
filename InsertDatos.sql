609b187fa9b0f795472d87b5e3310a8da521f702
Salchy <leandrocg524@gmail.com>
2025-06-10 14:51:32 -0300

    Iserción manual de ventas, para probar la vista creada

--- InsertDatos.sql
+++ InsertDatos.sql
@@ -1,3 +1,5 @@
+-- Insert de datos
+
 USE TPIGrupo29;
 
 INSERT INTO Marcas (Nombre) VALUES
@@ -24,9 +26,8 @@ EXEC SP_AgregarArticulo 'Netbook Latitude 3420', 3, 350000
 EXEC SP_AgregarArticulo 'Motorola G60s', 4, 200000
 EXEC SP_AgregarArticulo 'Notebook Ideapad 3', 5, 310000
 
-EXEC SP_AgregarArticulo 'Prueba', 3, -1
-
-SELECT * FROM Articulos;
+EXEC SP_AgregarArticulo 'Prueba', 3, -1; -- Este da error, y no permite insertarlo, ya que el precio es negativo
+EXEC SP_AgregarArticulo 'Prueba', 8, 5000; -- Este también da error, y no permite insertarlo, ya que el el id de marca no existe en la base de datos
 
 INSERT INTO Stock (IDArticulo, Cantidad) VALUES
 	(1, 8),
@@ -36,3 +37,12 @@ INSERT INTO Stock (IDArticulo, Cantidad) VALUES
 	(5, 8);
 
 SELECT * FROM Stock;
+
+INSERT INTO Ventas (Fecha, TipoFactura, ImporteTotal) VALUES -- Cómo calculo realmente el ImporteTotal? Debería insertar el importeTotal, el valor del precio del artículo, en el procedimiento de registrar una venta
+	(GETDATE(), 'B', 4000),
+	(GETDATE(), NULL, 5000),
+	(GETDATE(), 'A', 6000);
+
+INSERT INTO ArticulosVenta (IDVenta, IDArticulo, Cantidad, PrecioUnitario) VALUES (1, 2, 2, 550000);
+INSERT INTO ArticulosVenta (IDVenta, IDArticulo, Cantidad, PrecioUnitario) VALUES (2, 4, 2, 120000);
+INSERT INTO ArticulosVenta (IDVenta, IDArticulo, Cantidad, PrecioUnitario) VALUES (1, 5, 1, 310000);
