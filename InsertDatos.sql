f3a1e7be4fca859bc4bafdab52bf9b52c54c84d1
Salchy <leandrocg524@gmail.com>
2025-06-10 10:17:40 -0300

    Creacion de procedure, dar ade alta un producto

--- InsertDatos.sql
+++ InsertDatos.sql
@@ -9,12 +9,22 @@ INSERT INTO Marcas (Nombre) VALUES
 
 SELECT * FROM MARCAS;
 
-INSERT INTO Articulos (IDMarca, Nombre, Precio) VALUES
-	(1, 'Smart TV 55"', 250000),
-	(2, 'iPhone 14', 550000),
-	(3, 'Netbook Latitude 3420', 350000),
-	(4, 'Motorola G60s', 200000),
-	(5, 'Notebook Ideapad 3', 310000);
+-- Versión sin procedimiento
+--INSERT INTO Articulos (IDMarca, Nombre, Precio) VALUES
+--	(1, 'Smart TV 55"', 250000),
+--	(2, 'iPhone 14', 550000),
+--	(3, 'Netbook Latitude 3420', 350000),
+--	(4, 'Motorola G60s', 200000),
+--	(5, 'Notebook Ideapad 3', 310000);
+
+-- Versión con procedimiento almacenado
+EXEC SP_AgregarArticulo 'Smart TV 55"', 1, 250000
+EXEC SP_AgregarArticulo 'iPhone 14', 2, 550000
+EXEC SP_AgregarArticulo 'Netbook Latitude 3420', 3, 350000
+EXEC SP_AgregarArticulo 'Motorola G60s', 4, 200000
+EXEC SP_AgregarArticulo 'Notebook Ideapad 3', 5, 310000
+
+EXEC SP_AgregarArticulo 'Prueba', 3, -1
 
 SELECT * FROM Articulos;
 
--- procedimientos.sql
+++ procedimientos.sql
@@ -1,11 +1,24 @@
-CREATE PROCEDURE SP_AgregarArticulo(
+ALTER PROCEDURE SP_AgregarArticulo(
 		@nombreArticulo nvarchar(100),
 		@idMarca int,
 		@precioArticulo floaT
 	)
 AS
-	BEGIN
-	-- Ver tema de restricciones y manejo de errores (TRY-CATH)
+	BEGIN TRY
+		IF (@precioArticulo < 0) BEGIN
+			RAISERROR('No se puede insertar un precio de artículo negativo', 16, 1)
+		END
+		DECLARE @marcaID int
+		SET @marcaID = 0;
+
+		SELECT @marcaID = M.IDMarca FROM MARCAS AS M WHERE M.IDMarca = @idMarca;
+
+		IF (@marcaID = 0) BEGIN
+			RAISERROR('Número de ID de marca no existe en la base de datos', 16, 1)
+		END
 		INSERT INTO Articulos (Nombre, IDMarca, Precio) VALUES (@nombreArticulo, @idMarca, @precioArticulo);
-	END
+	END TRY
+	BEGIN CATCH
+		PRINT ERROR_MESSAGE();
+	END CATCH
 GO
